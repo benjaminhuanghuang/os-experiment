f8367cc35ae36589ad8f36dab56f9b6568bacab3
benjamin huang <benjaminhuanghuang@gmail.com>
2022-06-18 01:01:20 -0700

    fix

--- 03-c/loader.asm
+++ 03-c/loader.asm
@@ -1,20 +1,11 @@
 LOADER_BASE_ADDR EQU 0X900          	;加载第2个段的地址
 KERNEL_BASE_ADDR EQU 0X1500
 
-KERNEL_START_SECTOR EQU 0X9;kernel从第9个扇区开始读
-  SECTION LOADER VSTART=LOADER_BASE_ADDR
-
-  MOV AX, 0XB800        	;显存位置
-  MOV ES, AX
-
-  MOV BYTE[ES: 0X00], 'O'
-  MOV BYTE[ES: 0X01], 0X07
-  MOV BYTE[ES: 0X02], 'K'
-  MOV BYTE[ES: 0X03], 0X06
+SECTION LOADER VSTART=LOADER_BASE_ADDR
 
 load_kernel:
   mov DX, 0       ; DH:head, DL:driver
-  mov CX, 2       ; read section 3
+  mov CX, 3       ; read section 3
   mov AX, 0       ; read kernel to ES:BX (0x1500)
   mov ES, AX
   mov BX, 0x900
