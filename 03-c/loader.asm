b073fe2ead0d2776086705673aeefa73edb84035
benjamin huang <benjaminhuanghuang@gmail.com>
2022-06-18 00:42:58 -0700

    fix

--- 03-c/loader.asm
+++ 03-c/loader.asm
@@ -14,7 +14,7 @@ KERNEL_START_SECTOR EQU 0X9;kernel从第9个扇区开始读
 
 load_kernel:
   mov DX, 0       ; DH:head, DL:driver
-  mov CX, 2       ; read section 23
+  mov CX, 2       ; read section 3
   mov AX, 0       ; read kernel to ES:BX (0x900)
   mov ES, AX
   mov BX, 0x900
