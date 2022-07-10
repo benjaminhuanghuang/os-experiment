3c85ded0fbc06354be221e6b50f439945513c418
benjamin huang <benjaminhuanghuang@gmail.com>
2022-07-10 16:20:58 -0700

    protect mode

--- 03-protect-mode/kernel.asm
+++ 03-protect-mode/kernel.asm
@@ -1,91 +1,103 @@
 %include "pm.inc"
 
-org   0x9000
+org 0x9000
+jmp START
 
-jmp   LABEL_BEGIN
+[section .gdt]
+; GDT definition
+;                           段基址     段界限               属性
+GDT_ENTRY    :  Descriptor    0,       0,                  0
+CODE32_DESC  :  Descriptor    0,       Code32SegLen  - 1,  DA_C + DA_32
+; 在文本模式下的显存的地址范围映射位[0xB8000，0xBFFFF],一屏幕可以显示25行，每行80个字符
+VIDEO_DESC   :  Descriptor    0xB8000, 0x7FFF,             DA_DRW + DA_32
+; GDT end
 
-[SECTION .gdt]
- ;                                  段基址          段界限                属性
-LABEL_GDT:          Descriptor        0,            0,                   0  
-LABEL_DESC_CODE32:  Descriptor        0,      SegCode32Len - 1,       DA_C + DA_32
-LABEL_DESC_VIDEO:   Descriptor     0B8000h,         0ffffh,            DA_DRW
+GdtLen    equ   $ - GDT_ENTRY
+GdtPtr:
+  dw   GdtLen - 1
+  dd   0
 
-; length of GDT = current address - LABEL_GDT
-GdtLen     equ    $ - LABEL_GDT
-; 
-GdtPtr     dw     GdtLen - 1  ; 2 bytes, the limit of GDT
-           dd     0           ; 4 bytes, the start of GDT       
+; offset of the code32 selector
+SelectorCode32    equ   CODE32_DESC -  GDT_ENTRY
+; offset of the vido selector
+SelectorVideo     equ   VIDEO_DESC  -  GDT_ENTRY
 
-; address of the second selector
-SelectorCode32    equ   LABEL_DESC_CODE32 -  LABEL_GDT
-; address of the third selector
-SelectorVideo     equ   LABEL_DESC_VIDEO  -  LABEL_GDT
 
-[SECTION  .s16]
+[section .s16]
 [BITS  16]
-LABEL_BEGIN:
-     mov   ax, cs
-     mov   ds, ax
-     mov   es, ax
-     mov   ss, ax
-     mov   sp, 0100h
+START:
+  mov   ax, cs
+  mov   ds, ax
+  mov   es, ax
+  mov   ss, ax
+  mov sp, 0x900
 
-     xor   eax, eax
-     mov   ax,  cs
-     shl   eax, 4
-     add   eax, LABEL_SEG_CODE32
-     mov   word [LABEL_DESC_CODE32 + 2], ax
-     shr   eax, 16
-     mov   byte [LABEL_DESC_CODE32 + 4], al
-     mov   byte [LABEL_DESC_CODE32 + 7], ah
+; initialize GDT for 32 bits code segment
+  mov eax, 0
+  mov ax, cs
+  shl eax, 4
+  add eax, CODE32_SEGMENT
+  mov word [CODE32_DESC + 2], ax
+  shr eax, 16
+  mov byte [CODE32_DESC + 4], al
+  mov byte [CODE32_DESC + 7], ah
 
-     xor   eax, eax
-     mov   ax, ds
-     shl   eax, 4
-     add   eax,  LABEL_GDT
-     mov   dword  [GdtPtr + 2], eax
+; initialize GDT pointer struct
+    mov eax, 0
+    mov ax, ds
+    shl eax, 4
+    add eax, GDT_ENTRY
+    mov dword [GdtPtr + 2], eax
 
-     lgdt  [GdtPtr]
+; 1. Load GDT
+  lgdt  [GdtPtr]         ; set register
 
-     cli   ;关中断
+; 2. close interrupt 
+  cli                    ; 关中断
 
-     ; 92 号端口 (8位)的最高位值1
-     in    al,  92h
-     or    al,  00000010b
-     out   92h, al
-     ; 
-     mov   eax, cr0
-     or    eax , 1
-     mov   cr0, eax
+; 3. open A20, 92 号端口 (8位)的最高位值1
+  in    al,  92h
+  or    al,  00000010b
+  out   92h, al
 
-     jmp   dword  SelectorCode32: 0
+; 4. Set CR0 register
+  mov   eax, cr0
+  and		eax, 0x7fffffff	; 使用bit31（禁用分页）
+  or    eax , 1         ; set PE
+  mov   cr0, eax
 
-     [SECTION .s32]
-     [BITS  32]
-LABEL_SEG_CODE32:
-    mov   ax, SelectorVideo
-    mov   gs, ax
-    mov   si, msg
-    mov   ebx, 10
-    mov   ecx, 2
+; 5. Jump to 32bit code space
+  jmp   dword  SelectorCode32: 0      ; SelectorCode32 is the offset of code 32 selector
+
+[SECTION .s32]
+[BITS  32]
+CODE32_SEGMENT:
+  mov   ax, SelectorVideo
+  mov   gs, ax   ; 此时段寄存器gs的内容是 段描述符在 GDT 的偏移
+
+  mov   si, msg  
+  mov   ebx, 10  
+  mov   ecx, 2
+
 showChar:
-    mov   edi, (80*11)
-    add   edi, ebx
-    mov   eax, edi
-    mul   ecx
-    mov   edi, eax
-    mov   ah, 0ch
-    mov   al, [si]
-    cmp   al, 0
-    je    end
-    add   ebx,1
-    add   si, 1
-    mov   [gs:edi], ax
-    jmp    showChar
+  mov   edi, (80*11)  ;  屏幕第11行
+  add   edi, ebx
+  mov   eax, edi
+  mul   ecx
+  mov   edi, eax
+  mov   ah, 0x0c       ; 字符属性 0000 黑底, 1100: 红字 
+  mov   al, [si]
+  cmp   al, 0          ; 检查字符串结尾  \0
+  je    end
+  add   ebx,1
+  add   si, 1
+  mov   [gs:edi], ax
+  jmp   showChar
+
 end: 
     jmp   $
 msg:
     DB     "Protect Mode", 0
 
-SegCode32Len   equ  $ - LABEL_SEG_CODE32
+Code32SegLen   equ  $ - CODE32_SEGMENT
 
